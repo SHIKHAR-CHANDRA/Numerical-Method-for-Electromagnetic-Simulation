adb47c13b4446676c8a90856e13a6dcd78faff5a
Shikhar Chandra <47221817+SHIKHAR-CHANDRA@users.noreply.github.com>
2022-01-14 20:43:26 +0530

    Add files via upload

--- OnedFDTD.m
+++ OnedFDTD.m
@@ -40,7 +40,7 @@ UR(nz1:nz2)=ur;
 dt = (nbc*dz)/(2*c0); % wave travels 1 grid cell in 2 time steps
 
 tau = 0.5/fmax;
-t0 = 6*tau; % delay for source injection
+t0 = 5*tau; % delay for source injection
 
 MEy = (c0*dt)./ER; % update coefficient for Ey
 MHx = (c0*dt)./UR; % update coefficient for Hx
@@ -62,7 +62,7 @@ Esrc = exp(-((t-t0)/tau).^2); % Source Electric field
 Hsrc = A*exp(-((t-t0+delt)/tau).^2); % Source Magnetic Field
 
 % Initialize Fourier Transform
-NFREQ = 1000;
+NFREQ = 24001;
 FREQ = linspace(0, fmax, NFREQ);
 K = exp(-1i*2*pi*dt*FREQ); % Kernels for each frequency
 REF = zeros(1,NFREQ); % array for Reflectance spectrum
@@ -72,21 +72,11 @@ SRC = zeros(1,NFREQ); % array for Source spectrum
 z = linspace(0,Nz*dz,Nz); % 1-D position array
 
 E3=0;E2=0;E1=0;H3=0;H2=0;H1=0;
-% figure(1);
-% set(gcf,'doublebuffer','on'); %set double buffering on for smoother graphics
-% plot(Ey);
-% grid on;
 
-figure(1)
-x = [nz1*dz nz2*dz nz2*dz nz1*dz nz1*dz];
-y = [-3 -3 3 3 -3];
-fill(x,y,'g')
-axis([0 Nz*dz -3 3]);
-grid on;
-hold on;
 
 for T = 1:STEPS
-    
+
+
     % Update H from E using Dirichlet Boundary condition
     for nz = 1:Nz-1
         Hx(nz) = Hx(nz)+MHx(nz)*(Ey(nz+1)-Ey(nz))/dz;
@@ -97,7 +87,7 @@ for T = 1:STEPS
     
     H3 = H2; H2 = H1; H1 = Hx(1); % Record H at Perfect Absorbing Boundary
     
-    %Update E from H using Dirichlet Boundary condition
+    % Update E from H using Dirichlet Boundary condition
     Ey(1) = Ey(1)+MEy(1)*(Hx(1)-H3)/dz;
     
     for nz = 2:Nz
@@ -114,18 +104,25 @@ for T = 1:STEPS
         SRC(nf) = SRC(nf)+(K(nf)^T)*Esrc(T);
     end
    
-   if ~mod(T,3)
    figure(1)
+   set(gcf,'doublebuffer','on');
+   if ~mod(T,5)
+   x = [nz1*dz nz2*dz nz2*dz nz1*dz nz1*dz];
+   y = [-3 -3 3 3 -3];
+   fill(x,y,'g');
+   hold on
+   grid on;
+   axis([0 Nz*dz -3 3]);
    plot(z,Ey,'b');
-   hold on;
+   hold on
    plot(z,Hx,'r');
-   hold off;
+   hold off
    axis([0 Nz*dz -3 3]);
    title(['Iteration: ',num2str(T)]);
    xlabel('z (m)');
    ylabel('Field Amplitude');
-   drawnow;
    end
+   
    T
 end
 
@@ -136,14 +133,14 @@ SRC = SRC*dt;
 REF = abs(REF./SRC).^2;
 TRN = abs(TRN./SRC).^2;
 
-% figure(2);
-% plot(FREQ,REF,'-b');
-% hold on;
-% plot(FREQ,TRN,'-r');
-% plot(FREQ,REF+TRN,'-g');
-% xlim([FREQ(1) FREQ(NFREQ)]);
-% ylim([-0.0001 0.0005]);
-% xlabel('Frequency (Hz)');
-% title('REFLECTANCE AND TRANSMITANCE');
-% hold off;
+figure(2);
+plot(FREQ,REF,'-b');
+hold on;
+plot(FREQ,TRN,'-r');
+plot(FREQ,REF+TRN,'-g');
+xlim([FREQ(1) FREQ(NFREQ)]);
+ylim([0 2]);
+xlabel('Frequency (Hz)');
+title('REFLECTANCE AND TRANSMITANCE');
+hold off;
 
